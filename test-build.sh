522e4ef6fcb5b8448b76b31a7fa1e2e7f7fd18ae
Ted Mielczarek <ted@mielczarek.org>
2018-05-10 06:46:02 -0400

    switch to using --remap-path-prefix

--- test-build.sh
+++ test-build.sh
@@ -8,6 +8,14 @@ for path in /tmp/snippet-build-1 /tmp/snippet-build-2; do
     rm -rf "$path"
     mkdir "$path"
     cd "$path"
-    CARGO_TARGET_DIR="$PWD" RUSTFLAGS="-Zremap-path-prefix-from=$PWD -Zremap-path-prefix-to=/src" cargo +nightly build -v --manifest-path "$manifest"
-    sha1sum debug/deps/*.rlib
+    CARGO_TARGET_DIR="$PWD" RUSTFLAGS="--remap-path-prefix=$PWD=/src" cargo +nightly build -v --manifest-path "$manifest"
+    sha1sum debug/deps/*.rlib | sort > hashes
 done
+
+set +vx
+
+if diff -u /tmp/snippet-build-1/hashes /tmp/snippet-build-2/hashes; then
+    echo "Compiler outputs match!"
+else
+    echo "Compiler outputs do not match!"
+fi
